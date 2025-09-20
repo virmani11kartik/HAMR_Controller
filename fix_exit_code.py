4589789569e19fc2693c5bc87861189147ef9de7
kvirmani <kartik8virmani@gmail.com>
2025-09-20 03:02:54 -0400

    first version

--- fix_exit_code.py
+++
@@ -1,9 +0,0 @@
-# fix_exit_code.py
-Import("env")
-
-def after_upload(source, target, env):
-    print("Upload complete. Manual reset required.")
-    # Force success exit code (0)
-    env.Exit(0)
-
-env.AddPostAction("upload", after_upload)
